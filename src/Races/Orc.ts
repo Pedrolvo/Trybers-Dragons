8d19a4dc741a649842002b62ee72e38596f4fb70
Pedro Ivo <pedroivobrandaomattos@gmail.com>
2022-05-24 18:23:45 -0300

    feat(Races): Create class Orc

--- src/Races/Orc.ts
+++ src/Races/Orc.ts
@@ -1,14 +1,14 @@
 import Race from './Race';
 
-class Halfling extends Race {
+class Orc extends Race {
   private _maxLifePoints: number;
   private static _instances = 0;
 
   constructor(name: string, dexterity: number) {
     super(name, dexterity);
 
-    this._maxLifePoints = 60;
-    Halfling._instances += 1;
+    this._maxLifePoints = 74;
+    Orc._instances += 1;
   }
 
   public get maxLifePoints() {
@@ -16,8 +16,8 @@ class Halfling extends Race {
   }
 
   public static createdRacesInstances(): number {
-    return Halfling._instances;
+    return Orc._instances;
   }
 }
 
-export default Halfling;
+export default Orc;
